c4b004c92aa6607b6466dcd111212a632d7b2f4b
nadiannis <annisanadianeyla@gmail.com>
2022-06-23 15:52:52 +0700

    test: create sample test

---
+++ app/app/tests.py
@@ -0,0 +1,14 @@
+'''
+Sample Tests
+'''
+
+from django.test import SimpleTestCase 
+from . import calc 
+
+class CalcTests(SimpleTestCase):
+  ''' Test the calc module '''
+
+  def test_add_numbers(self):
+    res = calc.add(7, 5)
+
+    self.assertEqual(res, 12)
