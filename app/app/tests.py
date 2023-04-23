cf700169fe3af140620d0adf1ae6f167dc8f56fb
nadiannis <annisanadianeyla@gmail.com>
2023-04-24 00:40:07 +0700

    test: create add numbers test as a sample test

---
+++ app/app/tests.py
@@ -0,0 +1,16 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module."""
+
+    def test_add_numbers(self):
+        """Test adding numbers together."""
+        res = calc.add(20, 4)
+
+        self.assertEqual(res, 24)
