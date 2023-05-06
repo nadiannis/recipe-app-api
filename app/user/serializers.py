e83207a11d847c922f10d2c1adee6e19499e18d1
nadiannis <annisanadianeyla@gmail.com>
2023-05-07 00:27:58 +0700

    feat: implement user login

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,7 +1,8 @@
 """
 Serializers for the user API view.
 """
-from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate, get_user_model
+from django.utils.translation import gettext as _
 from rest_framework import serializers
 
 
@@ -16,3 +17,29 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create & return a user with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user auth token."""
+
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+    )
+
+    def validate(self, attrs):
+        """Validate & authenticate the user."""
+        email = attrs.get('email')
+        password = attrs.get('password')
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password,
+        )
+        if not user:
+            message = _('Unable to authenticate with provided credentials.')
+            raise serializers.ValidationError(message, code='authorization')
+
+        attrs['user'] = user
+        return attrs
