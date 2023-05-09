2eb202a209d53fa675e9c094840ec3967955e6fe
nadiannis <annisanadianeyla@gmail.com>
2023-05-09 21:31:04 +0700

    feat: implement recipe listing API

---
+++ app/recipe/serializers.py
@@ -0,0 +1,14 @@
+"""
+Serializers for recipe API.
+"""
+from core.models import Recipe
+from rest_framework import serializers
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for recipes."""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        read_only_fields = ['id']
