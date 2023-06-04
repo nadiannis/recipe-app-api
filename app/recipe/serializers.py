847786915c4d8773a5ddff69eb8881d5ba3e8c7d
nadiannis <annisanadianeyla@gmail.com>
2023-06-05 00:48:02 +0700

    refactor: turn getting or creating tags into a separate method

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -25,15 +25,19 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
         read_only_fields = ['id']
 
-    def create(self, validated_data):
-        """Create a recipe."""
-        tags = validated_data.pop('tags', [])
-        recipe = Recipe.objects.create(**validated_data)
+    def _get_or_create_tags(self, tags, recipe):
+        """Handle getting or creating tags as needed."""
         auth_user = self.context['request'].user
 
         for tag in tags:
             tag_obj, created = Tag.objects.get_or_create(user=auth_user, **tag)
             recipe.tags.add(tag_obj)
+
+    def create(self, validated_data):
+        """Create a recipe."""
+        tags = validated_data.pop('tags', [])
+        recipe = Recipe.objects.create(**validated_data)
+        self._get_or_create_tags(tags, recipe)
 
         return recipe
 
@@ -43,11 +47,7 @@ class RecipeSerializer(serializers.ModelSerializer):
 
         if tags is not None:
             instance.tags.clear()
-            auth_user = self.context['request'].user
-
-            for tag in tags:
-                tag_obj, created = Tag.objects.get_or_create(user=auth_user, **tag)
-                instance.tags.add(tag_obj)
+            self._get_or_create_tags(tags, instance)
 
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
